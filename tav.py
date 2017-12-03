7ba57df4011640c540276a9562d204fecf601c5b
mudox <imudox@gmail.com>
2017-12-03 09:31:43 +0800

    Turn actions as sub-parsers

--- tav.py
+++ tav.py
@@ -3,79 +3,42 @@
 
 
 import argparse
-import shlex
-import shutil
 import subprocess
 from pathlib import Path
 
-from snapshot import TmuxSnapshot
+from core import choose_tree, update
 
-out_dir = Path('~/.local/share/tmux-choose-tree').expanduser()
+out_dir = Path('~/.local/share/tav').expanduser()
 out_dir.mkdir(parents=True, exist_ok=True)
-feed_path = out_dir / 'fzf-feed'
+fzf_feed_path = out_dir / 'fzf-feed'
 width_path = out_dir / 'width'
 update_path = out_dir / 'update'
 
 
-def update():
-  snapshot = TmuxSnapshot()
+def snapshot(args):
+  update()
 
-  with open(str(feed_path), 'w') as file:
-    file.write(snapshot.fzf_lines())
 
-  with open(str(width_path), 'w') as file:
-    file.write(str(snapshot.width))
+def oneshot(args):
+  update()
+  choose_tree(oneshot=True)
 
-def choose(once):
-  t_width, t_height = shutil.get_terminal_size()
-  with open(width_path) as file:
-    width = int(file.read())
 
-  h_margin = int((t_width - width) / 2) - 3
-  t_margin = 3
+def serve(args):
+  while True:
+    choose_tree(oneshot=False)
 
-  cmd = shlex.split(f'''
-    fzf
-    --exact
 
-    # hide prefixing id's
-    --with-nth=2..
-
-    # retain the relationship between matched sessions and windows
-    --tiebreak=index
-
-    # disable all quit bindings
-
-    --ansi
-    --height=100%
-
-    # center interface in the terminal screen
-    --margin={t_margin},{h_margin}
-
-    # fully transparent background
-    --color=bg:-1,bg+:-1
-  ''', comments=True)
-
-  if not once:
-    cmd.append('--no-clear')
-    for key in ('esc', 'ctrl-c', 'ctrl-g', 'ctrl-q'):
-      cmd.append(f'--bind={key}:execute(tmux switch-client -p)')
-
-  with open(str(feed_path)) as file:
-    process = subprocess.run(cmd, stdin=file, stdout=subprocess.PIPE)
-    if process.returncode != 0:
-      return
-
-    result = process.stdout.decode().split()
-    if len(result) == 0:  # may select an empty line
-      return
-
-    target_id = result[0]
-    subprocess.run(['tmux', 'switch-client', '-t', target_id])
+def hook(args):
+  if args.hook_enabled is None:
+    hook_update()
+  else:
+    enable_hook_update(args.hook_enabled)
 
 
 def hook_update():
   if not is_hook_update_enabled():
+    print('disabled')
     return
   else:
     update()
@@ -93,39 +56,67 @@ def is_hook_update_enabled():
       return True
 
 
-def set_hook_update(enable):
+def enable_hook_update(flag):
   with open(str(update_path), 'w') as file:
-    file.write(enable and 'yes' or 'no')
+    file.write(flag and 'yes' or 'no')
 
 
-def main():
-  cliParser = argparse.ArgumentParser(
+def parse_args():
+  parser = argparse.ArgumentParser(
       prog='tav',
-      description='An tmux choose-tree alternative powered by fzf'
+      description='An tmux `choose-tree` replacement powered by fzf command.'
   )
-  cliParser.add_argument(
-      'action',
-      nargs='?',
-      default='update and choose once',
-      help='one of update|once|interface'
+  parser.set_defaults(func=oneshot)  # defaults to update and choose once
+  subparsers = parser.add_subparsers(
+      title='actions',
+      description='without any action is equivalent to `oneshot`'
   )
-  options = cliParser.parse_args()
 
-  if options.action == 'update':
-    update()
-  elif options.action == 'once':
-    choose(once=True)
-  elif options.action == 'hook':
-    hook_update()
-  elif options.action == 'interface':
-    while True:
-      choose(once=False)
-  elif options.action == 'update and choose once':
-    update()
-    choose(once=True)
-  else:
-    cliParser.print_help()
+  # command `update`
+  act_snapshot = subparsers.add_parser(
+      'snapshot', aliases=['snp'],
+      help='create a new snapshot tmux session window layout.'
+  )
+  act_snapshot.set_defaults(func=snapshot)
+
+  # command `hook`
+  act_hook = subparsers.add_parser(
+      'hook', aliases=['h', 'hk', 'ho'],
+      help='update snapshot and update the fzf interface in tmux window `Tmux:Navigator`.'
+  )
+  group = act_hook.add_mutually_exclusive_group()
+  group.add_argument(
+      '-d, --disable',
+      dest='hook_enabled',
+      action='store_false',
+      help='disable background updating triggered by tmux hooks'
+  )
+  group.add_argument(
+      '-e, --enable',
+      dest='hook_enabled',
+      action='store_true',
+      help='enable background updating'
+  )
+  act_hook.set_defaults(func=hook, hook_enabled=None)
+
+  # command `oneshot`
+  act_oneshot = subparsers.add_parser(
+      'oneshot', aliases=['o', 'os'],
+      help='make a new snapshot and show the fzf interface, close after choose and switch. this is the DEFAULT action'
+  )
+  act_oneshot.set_defaults(func=oneshot)
+
+  # command `serve`
+  act_serve = subparsers.add_parser(
+      'serve', aliases=['srv'],
+      help='show the fzf inteface, remain after choose and switch.'
+  )
+  act_serve.set_defaults(func=serve)
+
+  # options = parser.parse_args()
+  args = parser.parse_args()
+  args.func(args)
 
 
 if __name__ == "__main__":
-  main()
+  parse_args()
