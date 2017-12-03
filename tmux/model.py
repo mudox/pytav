5d4febba648c365783c99d341d5ee4a745482574
mudox <imudox@gmail.com>
2017-12-04 01:39:20 +0800

    Define specific classes for tmux components into `tmux/model.py`

---
+++ tmux/model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from typing import NamedTuple
+
+
+class Window(NamedTuple):
+  id: str
+  name: str
+
+
+class Session(NamedTuple):
+  name: str
+  loaded: bool
+  id: str = None
+  windows: [Window] = None
