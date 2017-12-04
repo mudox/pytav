ea62775427c48d77ff10b67a12afd21fb8e9a55c
mudox <imudox@gmail.com>
2017-12-04 08:37:21 +0800

    Add dead session collecting logic to `tmux.Snapshot`

--- tmux/snapshot.py
+++ tmux/snapshot.py
@@ -5,6 +5,7 @@ import shlex
 import shutil
 import subprocess
 
+import settings
 import tmux
 
 
@@ -30,7 +31,13 @@ class Snapshot:
     '''
       take a snapshot
     '''
-    result = {}
+    self.all_sessions = []  # list of tmux.Session objects
+
+    #
+    # scan live sessions
+    #
+
+    live_sessions = []
     w_width = 0  # max window name width
     s_width = 0  # max session name width
 
@@ -40,8 +47,8 @@ class Snapshot:
     for sline in slines:
       sid, sname = sline.split(':')
       session = tmux.Session(id=sid, name=sname, loaded=True, windows=[])
-      result[sid] = session
 
+      live_sessions.append(session)
       s_width = max(s_width, len(sname))
 
       cmd = list_windows_cmd + [sid]
@@ -50,23 +57,34 @@ class Snapshot:
 
       for wline in wlines:
         wid, wname = wline.split(':')
-
-        # filter out all Tav window
-        if sname == 'Tmux' and wname == 'Navigator':
-          continue
-
         window = tmux.Window(id=wid, name=wname)
+
         session.windows.append(window)
-
         w_width = max(w_width, len(wname))
-
-      if len(session.windows) == 0:
-        assert session.name == 'Tmux'
-        del result[session.id]
 
     # widths
     t_width, t_height = shutil.get_terminal_size()
 
     self.w_width = w_width
     self.s_width = s_width
-    self.session_map = result
+
+    self.live_sessions = live_sessions
+    self.all_sessions += live_sessions
+
+    #
+    # scan dead sessions
+    #
+
+    live_snames = [s.name for s in self.live_sessions]
+
+    snames = [
+        x.stem for x in settings.paths.sessions.glob('*')
+        if not x.stem.startswith('.')]
+
+    dead_snames = [n for n in snames if n not in live_snames]
+
+    self.dead_session = [
+        tmux.Session(
+            name=n,
+            loaded=False) for n in dead_snames]
+    self.all_sessions += self.dead_session
