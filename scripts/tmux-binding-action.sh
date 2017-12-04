2ba4de8ae50a8ddaf62bded672d9cfc2d8a480d4
mudox <imudox@gmail.com>
2017-12-05 05:52:38 +0800

    Rename script files, use `-` over `_`

--- scripts/tmux-binding-action.sh
+++ scripts/tmux-binding-action.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 script_dir=$(dirname $(test -L "$0" && readlink -e "$0" || echo "$0"))
-"${script_dir}/prepare_tmux_interface.sh" nokill &>/dev/null
+"${script_dir}/prepare-tmux-interface.sh" nokill &>/dev/null
 tmux switch-client -t "Tav:Finder"
