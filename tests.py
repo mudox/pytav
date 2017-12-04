738090f83a6a63504deca71ce11f9ef0a0d7b21b
mudox <imudox@gmail.com>
2017-12-05 04:25:30 +0800

    Add `test_prepare_tmux_interface`

--- tests.py
+++ tests.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from fzf_feeder import FZFFormatter
+import core
+import fzf_feeder
 import settings
-
 import tmux
 
 
 def test(args):
   settings.action = 'test'
   test_settings()
-  # test_snapshot()
+  test_snapshot()
   test_formatter()
+  test_prepare_tmux_interface()
+
 
 def _title(text):
   print(f'\n\033[35m{text}\033[0m\n')
+
 
 def test_settings():
   _title('Test [settings] ...')
   tty = settings.log_tty()
   print(f'log window tty: {tty}')
+
 
 def test_snapshot():
   _title('Test [snapshot] ...')
@@ -31,5 +35,11 @@ def test_snapshot():
 def test_formatter():
   _title('\nTest [formatter] ...')
   snap = tmux.Snapshot()
-  formatter = FZFFormatter(snap)
+  formatter = fzf_feeder.FZFFormatter(snap)
   print(formatter.fzf_lines())
+
+
+def test_prepare_tmux_interface():
+  _title('\nTest [prepare tmux interface] ...')
+  core.prepare_tmux_interface(force=False)
+  core.prepare_tmux_interface(force=True)
