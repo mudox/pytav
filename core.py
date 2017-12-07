41efb5932368bd100800c83cec19bb63802a759c
mudox <imudox@gmail.com>
2017-12-07 08:05:33 +0800

    Refactor `strat_ui` to read pid based json file

--- core.py
+++ core.py
@@ -59,12 +59,19 @@ def start_ui(oneshot):
   compose fzf command line, show it centered in current terimnal secreen.
   '''
 
+  pid = tmux.get_server_pid()
+  with (settings.paths.serve_dir / str(pid)).open() as file:
+    info = json.load(file)
+
+
+  # center fzf ui
   t_width, t_height = shutil.get_terminal_size()
-  width = int(settings.paths.width.read_text())
+  width = info['fzf']['ui_width']
 
   h_margin = int((t_width - width) / 2) - 3
   t_margin = 3
 
+  # compose fzf command line
   cmd = shlex.split(f'''
     fzf
     --exact
@@ -75,10 +82,8 @@ def start_ui(oneshot):
     # retain the relationship between matched sessions and windows
     --tiebreak=index
 
-    # disable all quit bindings
-
-    --ansi
-    --height=100%
+    --ansi          # color mode
+    --height=100%   # fullscreen mode
 
     # center interface in the terminal screen
     --margin={t_margin},{h_margin}
@@ -100,74 +105,75 @@ def start_ui(oneshot):
     # prevent ctrl-z suspend fzf
     cmd.append('--bind=ctrl-z:unix-line-discard')
 
-  with settings.paths.fzf_feed.open() as file:
+  #
+  # show fzf interface, get user selection
+  #
 
-    #
-    # show fzf interface, get user selection
-    #
+  lines = info['fzf']['lines']
 
-    process = subprocess.run(
-        cmd,
-        stdin=file,
-        stdout=subprocess.PIPE
-    )
+  process = subprocess.run(
+      cmd,
+      input=lines.encode(),
+      stdout=subprocess.PIPE
+  )
 
-    if process.returncode != 0:
-      # TODO!: alert error, wait for a key to continue
-      return
+  if process.returncode != 0:
+    # TODO!: alert error, wait for a key to continue
+    return
 
-    tag = process.stdout.decode().split('\t')[0].strip()
+  tag = process.stdout.decode().split('\t')[0].strip()
 
-    #
-    # handle the tag
-    #
+  #
+  # handle the tag
+  #
 
-    # empty separate line
-    if len(tag) == 0:
-      return
+  # empty separate line
+  if len(tag) == 0:
+    return
 
-    # live session or window line
-    elif tag.startswith('$') or tag.startswith('@'):
-      subprocess.run(['tmux', 'switch-client', '-t', tag])
+  # live session or window line
+  elif tag.startswith('$') or tag.startswith('@'):
+    subprocess.run(['tmux', 'switch-client', '-t', tag])
 
-    # other auxiliary lines, e.g. dead sessions group line
-    elif tag == '<nop>':
-      return
+  # other auxiliary lines, e.g. dead sessions group line
+  elif tag == '<nop>':
+    return
 
-    # dead session line
-    else:
-      try:
+  # handle dead session line
+  # TODO!!: split actions into methods
+  else:
+    try:
 
-        #
-        # show creating message
-        #
+      #
+      # show creating message
+      #
 
-        subprocess.run('clear')                        # clear screen
-        subprocess.run(f'tput civis'.split())          # hide cursor
-        x = int(t_width / 2 - 12)
-        y = int(t_height / 2)
-        subprocess.run(f'tput cup {y} {x}'.split())    # center message
+      subprocess.run('clear')                        # clear screen
+      subprocess.run(f'tput civis'.split())          # hide cursor
+      x = int(t_width / 2 - 12)
+      y = int(t_height / 2)
+      subprocess.run(f'tput cup {y} {x}'.split())    # center message
 
-        print(f'\033[33mCreating session [{tag}] ...\033[0m', end=None)
+      print(f'\033[33mCreating session [{tag}] ...\033[0m', end=None)
 
-        #
-        # create session
-        #
+      #
+      # create session
+      #
 
-        tmux.hook.enable(False)                        # disable hook updating
+      tmux.hook.enable(False)                        # disable hook updating
 
-        path = settings.paths.sessions / tag           # create session
-        subprocess.run(
-            str(path),
-            stdout=subprocess.DEVNULL
-        )
+      path = settings.paths.sessions / tag           # create session
+      subprocess.run(
+          str(path),
+          stdout=subprocess.DEVNULL
+      )
 
-        update()                                       # update snapshot
+      update()                                       # update snapshot
 
-        sleep(1)                                       # warming
+      sleep(1)                                       # warming
 
-        subprocess.call(['tmux', 'switch-client', '-t', tag])
+      subprocess.call(['tmux', 'switch-client', '-t', tag])
 
-      finally:
-        tmux.hook.enable(True)
-        subprocess.run(['tput', 'cnorm'])
+    finally:
+      tmux.hook.enable(True)
+      subprocess.run(['tput', 'cnorm'])
