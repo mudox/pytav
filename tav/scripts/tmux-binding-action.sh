a267ed5bac39ea30ef2ad2ddc476d9e6edec6954
mudox <imudox@gmail.com>
2017-12-10 07:10:33 +0800

    Add `realpath` to scripts/tmux-binding-action.sh

--- tav/scripts/tmux-binding-action.sh
+++ tav/scripts/tmux-binding-action.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-script_dir=$(dirname $(test -L "$0" && readlink -e "$0" || echo "$0"))
+script_dir=$(realpath $(dirname $(test -L "$0" && readlink -e "$0" || echo "$0")))
 "${script_dir}/prepare-tmux-interface.sh" nokill &>/dev/null
 tmux switch-client -t "Tav:Finder"
