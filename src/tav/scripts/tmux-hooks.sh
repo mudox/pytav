a3f7f756759ebf794c156511ffe2229f052cca8e
mudox <imudox@gmail.com>
2017-12-10 13:32:40 +0800

    Move app files into `src` folder

--- src/tav/scripts/tmux-hooks.sh
+++ src/tav/scripts/tmux-hooks.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 s_dir=$(dirname $(test -L "$0" && readlink -e "$0" || echo "$0"))
 
 for event in window-linked window-renamed window-unlinked; do
-  tmux set-hook -g "${event}" "run-shell '${s_dir}/../tav.py hook ${event}'"
+  tmux set-hook -g "${event}" "run-shell 'tav hook ${event}'"
 done
