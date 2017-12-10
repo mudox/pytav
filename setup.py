6898761001c4ad75f6afc8a8d42224682109578d
mudox <imudox@gmail.com>
2017-12-10 10:53:55 +0800

    Initial setup.py

--- setup.py
+++ setup.py
@@ -5,30 +5,25 @@
 
 
 import re
+
 from setuptools import setup
 
-
 version = re.search(
-    '^__version__\s*=\s*"(.*)"',
-    open('bootstrap/bootstrap.py').read(),
+    r"^__version__\s*=\s*'(.*)'",
+    open('tav/tav.py').read(),
     re.M
-    ).group(1)
-
-
-with open("README.rst", "rb") as f:
-    long_descr = f.read().decode("utf-8")
+).group(1)
 
 
 setup(
-    name = "cmdline-bootstrap",
-    packages = ["bootstrap"],
-    entry_points = {
-        "console_scripts": ['bootstrap = bootstrap.bootstrap:main']
-        },
-    version = version,
-    description = "Python command line application bare bones template.",
-    long_description = long_descr,
-    author = "Jan-Philip Gehrcke",
-    author_email = "jgehrcke@googlemail.com",
-    url = "http://gehrcke.de/2014/02/distributing-a-python-command-line-application",
-    )
+    name='tav',
+    packages=['tav'],
+    entry_points={
+        "console_scripts": ['tav = tav.tav:main']},
+    version=version,
+    description='TBD',
+    long_description='TBD',
+    author='Mudox',
+    author_email='imudox@gmail.com',
+    url='https://github.com/mudox/tav'
+)
