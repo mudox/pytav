3f679750844b41af4ddad2f46894fc330d64d44e
mudox <imudox@gmail.com>
2017-12-16 18:47:45 +0800

    Add `test_listAllWindows`

--- test/test_tmux.py
+++ test/test_tmux.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tav import settings, tmux
+from tav import tmux
 
 
-def test_serverPID():
-  assert settings.serverPID is not None
-
-
-def test_logTTY():
-  assert settings.paths.logTTY is not None
-  assert settings.paths.logTTY.exists()
+def test_listAllWindows():
+  assert tmux.listAllWindows() != ''
