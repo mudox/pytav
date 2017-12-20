3c05a45ee9e5d59e0bcb083cae01a3fbc7badef7
mudox <imudox@gmail.com>
2017-12-20 14:58:55 +0800

    Update `.tmux-session.sh` with new Log window layout

--- .tmux-session.sh
+++ .tmux-session.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 source ~/Git/dot-files/bash/lib/jack
 
 tput civis
-trap 'tput cnorm' EXIT
+tav hook -d
+trap 'tput cnorm; tav hook -e; tav hook after-enabled' EXIT
 
 # tty size
 set +u
@@ -29,7 +30,6 @@ fi
 #
 jackProgress 'Creating window [Edit] ...'
 
-
 root="${HOME}/Develop/Python/tav"
 window_name='Edit'
 window="${session_name}:${window_name}"
@@ -49,17 +49,12 @@ tmux split-window  \
   -c "${root}"
 tmux select-pane -t "${window}.1"
 
-
-tmux select-window -t "${session_name}:1.1"
-echo "[${session_name}]"
-tmux list-window -t "${session_name}" -F ' - #W'
-
 #
 # window: Log
 #
 jackProgress 'Creating window [Log] ...'
 
-root="${HOME}/.local/share/tav/log"
+root="${HOME}/.local/share/tav"
 window_name='Log'
 window="${session_name}:${window_name}"
 tmux new-window              \
@@ -67,11 +62,40 @@ tmux new-window              \
   -t "${session_name}:{end}" \
   -n "${window_name}"        \
   -c "${root}"               \
-  -d
-sleep 1
-tmux send-keys -t "${window}" "
-tail -f log
-"
+  -d                         \
+  'tput civis; tail -f log/tav.log'
+
+tmux select-pane -t "${window}.1" -d
+
+#
+# pane: Log.2
+# at the top right corner
+# tail the update file
+#
+
+root="${HOME}/.local/share/tav"
+tmux split-window  \
+  -t "${window}.1" \
+  -h               \
+  -l 60            \
+  -c "${root}"     \
+  'tput civis; tail -n 0 -f update'
+
+tmux select-pane -t "${window}.2" -d
+
+#
+# pane: Log.3
+# at the bottom right corner
+# for running testing commands
+#
+
+root="${HOME}/Develop/Python/tav"
+tmux split-window  \
+  -t "${window}.2" \
+  -v               \
+  -c "${root}"
+
+tmux select-pane -t "${window}.3"
 
 jackEndProgress
 tmux select-window -t "${session_name}:1.1"
