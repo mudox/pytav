d951f308820f6aa79373a9b2a89d3c93b77b844f
mudox <imudox@gmail.com>
2017-12-23 01:20:30 +0800

    Filter out tav session in snapshot

--- src/tav/tmux/snapshot.py
+++ src/tav/tmux/snapshot.py
@@ -4,8 +4,7 @@
 from functools import reduce
 from itertools import groupby
 
-from .. import settings
-from .. import tmux
+from .. import settings, tmux
 
 
 class Snapshot:
@@ -30,12 +29,14 @@ class Snapshot:
     # scan live sessions
     #
 
-    window_info_tuples = tmux.listAllWindows()
+    infoTuples = tmux.listAllWindows()
 
     # get tmux server id
 
     self.liveSessions = []
-    groups = groupby(window_info_tuples, lambda x: (x[0], x[1]))
+    # filter out tav session
+    tuples = filter(lambda x: x[1] != settings.tmux.tavSessionName, infoTuples)
+    groups = groupby(infoTuples, lambda x: (x[0], x[1]))
     for (sid, sname), value in groups:
       session = tmux.Session(id=sid, name=sname, loaded=True, windows=[])
       for _, _, wid, wname, windex in value:
@@ -43,8 +44,10 @@ class Snapshot:
 
       self.liveSessions.append(session)
 
-    self.sessionNameMaxWidth = reduce(max, [len(t[1]) for t in window_info_tuples])
-    self.windowNameMaxWidth = reduce(max, [len(t[3]) for t in window_info_tuples])
+    self.sessionNameMaxWidth = reduce(
+        max, [len(t[1]) for t in infoTuples])
+    self.windowNameMaxWidth = reduce(
+        max, [len(t[3]) for t in infoTuples])
 
     self.liveSessionCount = len(self.liveSessions)
     windowCounts = [len(s.windows) for s in self.liveSessions]
@@ -76,11 +79,12 @@ class Snapshot:
 
     self.deadSessions = []
     for name in dead_snames:
-      session = tmux.Session(id='<dead>', name=name, loaded=False, windows=None)
+      session = tmux.Session(
+          id='<dead>',
+          name=name,
+          loaded=False,
+          windows=None)
       self.deadSessions.append(session)
 
     self.allSessions += self.deadSessions
     self.deadSessionCount = len(self.deadSessions)
-
-
-
