ebe9b65e3be897ed3b8ad38125de1150621bfaf9
mudox <imudox@gmail.com>
2017-12-24 17:50:39 +0800

    Rename `tests.py` -> `diagnose.py`

--- src/tav/diagnose.py
+++ src/tav/diagnose.py
@@ -4,19 +4,16 @@
 from . import fzf, settings, tmux
 
 
-def test(args):
-  settings.action = 'test'
-
-  test_snapshot()
-  test_formatter()
-  # test_prepare_tmux_interface()
+def diagnose(args):
+  _snapshot()
+  _formatter()
 
 
 def _title(text):
   print(f'\n\033[35m{text}\033[0m\n')
 
 
-def test_snapshot():
+def _snapshot():
   _title('Test [snapshot] ...')
   snap = tmux.Snapshot()
   for s in snap.allSessions:
@@ -28,14 +25,14 @@ def test_snapshot():
   print(f' server pid: {snap.serverPID}')
 
 
-def test_formatter():
+def _formatter():
   _title('\nTest [formatter] ...')
   snap = tmux.Snapshot()
   formatter = fzf.FZFFormatter(snap, testMode=True)
   print(formatter.fzfFeed)
 
 
-def test_prepare_tmux_interface():
+def _prepare_tmux_interface():
   _title('\nTest [prepare tmux interface] ...')
   tmux.prepareTmuxInterface(force=False)
   tmux.prepareTmuxInterface(force=True)
