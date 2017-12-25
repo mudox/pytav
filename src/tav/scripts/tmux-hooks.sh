b537be50a5b64cf99e6352fa2e9d38e104672b6d
mudox <imudox@gmail.com>
2017-12-25 14:05:49 +0800

    Remove `scripts/tmux-hooks.sh`, use `tav hook -e` instead

--- src/tav/scripts/tmux-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-s_dir=$(dirname $(test -L "$0" && readlink -e "$0" || echo "$0"))
-
-for event in window-linked window-renamed window-unlinked session-renamed; do
-  tmux set-hook -g "${event}" "run-shell 'tav hook ${event}'"
-done
