48651fbf992bb37c48f15e32480eb8420c1be5fa
mudox <imudox@gmail.com>
2017-12-26 10:38:34 +0800

    Add equality operator implementation to `tmux.model`

--- src/tav/tmux/model.py
+++ src/tav/tmux/model.py
@@ -8,9 +8,37 @@ class Window(NamedTuple):
   name: str
   index: int
 
+  def __eq__(self, rhs):
+    if not isinstance(rhs, Window):
+      return NotImplemented
+
+    if self is rhs:
+      return True
+
+    return                         \
+        self.id == rhs.id and      \
+        self.name == rhs.name and  \
+        self.index == rhs.index
+
 
 class Session(NamedTuple):
   name: str
   loaded: bool
   id: str
   windows: list
+
+  def __eq__(self, rhs):
+    if not isinstance(rhs, Session):
+      return NotImplemented
+
+    if self is rhs:
+      return True
+
+    for a in ('id', 'loaded', 'name'):
+      if getattr(self, a) != getattr(rhs, a):
+        return False
+
+    if len(self.windows) != len(rhs.windows):
+      return False
+
+    return self.windows == rhs.windows
