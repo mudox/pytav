7672c3595bc1dec43aa2868e21b8e3647eb391f8
mudox <imudox@gmail.com>
2017-12-26 10:40:01 +0800

    Log to separate `server.log` and `interface.log`

--- src/tav/__init__.py
+++ src/tav/__init__.py
@@ -1,3 +1,15 @@
+import logging
+
 from jaclog import jaclog
 
-jaclog.configure(appName='tav', compact=True)
+from sys import argv
+
+if argv[1] == 'server':
+  jaclog.configure(appName='tav', fileName='server.log', compact=True)
+elif argv[1] == 'interface':
+  jaclog.configure(appName='tav', fileName='interface.log', compact=True)
+else:
+  jaclog.configure(appName='tav', compact=True)
+
+logging.getLogger('tav.tmux.agent').setLevel(logging.WARNING)
+logging.getLogger('tav.fzf').setLevel(logging.WARNING)
