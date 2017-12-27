0cfc0756a531bb15c11ef5b7146d170eff072637
mudox <imudox@gmail.com>
2017-12-27 17:30:19 +0800

    Improve logging init code

--- src/tav/__init__.py
+++ src/tav/__init__.py
@@ -4,12 +4,15 @@ from jaclog import jaclog
 
 from sys import argv
 
-if argv[1] == 'server':
-  jaclog.configure(appName='tav', fileName='server.log', compact=True)
-elif argv[1] == 'interface':
-  jaclog.configure(appName='tav', fileName='interface.log', compact=True)
+if len(argv) > 1:
+  if argv[1] == 'server':
+    jaclog.configure(appName='tav', fileName='server.log', compact=True)
+  elif argv[1] == 'interface':
+    jaclog.configure(appName='tav', fileName='interface.log', compact=True)
+  else:
+    jaclog.configure(appName='tav', compact=True)
 else:
   jaclog.configure(appName='tav', compact=True)
 
-logging.getLogger('tav.tmux.agent').setLevel(logging.WARNING)
+# logging.getLogger('tav.tmux.agent').setLevel(logging.WARNING)
 logging.getLogger('tav.fzf').setLevel(logging.WARNING)
