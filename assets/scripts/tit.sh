6360aa7274ee8687834b2a788a0c95bf49045f54
mudox <imudox@gmail.com>
2017-12-28 10:52:42 +0800

    Update `tit.sh`, use `tavs restart`

--- assets/scripts/tit.sh
+++ assets/scripts/tit.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 PS4="
 🚩  "
-set -x
+set -euxo pipefail
 
 root="${HOME}/Develop/Python/tav"
-pip3 install --no-deps -U "${root}" &&                                 \
-  pytest -v "${root}" &&                                               \
-  tmux send-keys -t 'Tav-Project:Server.2' c-c c-c 'tavs start' c-m && \
-  tav session recreate &&                                              \
-  tavs event after-install
+pip3 install --no-deps -U "${root}"
+pytest -v "${root}"
+tmux send-keys -t 'Tav-Project:Server.2' c-c c-c ' tavs restart' c-m
+sleep 1
+tavs event after-install
