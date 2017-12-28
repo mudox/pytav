58f6a112173ff6eb39590cdc3de24da983fc4641
mudox <imudox@gmail.com>
2017-12-28 17:29:29 +0800

    Refactor ans simplify `tavSession.create`, swap sessions instead of windows

--- src/tav/tmux/tavSession.py
+++ src/tav/tmux/tavSession.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 def isReady():
   out = shell.getStdout(f'''
-      tmux list-panes -t ={cfg.tmux.tavFrontWindowTarget} -F '#{{pane_current_command}}'
+      tmux list-panes -t ={cfg.tmux.tavWindowTarget} -F '#{{pane_current_command}}'
   ''')
 
   if out is None:
@@ -40,13 +40,11 @@ def create():
 
   sname = cfg.tmux.tavSessionName
 
-  fname = cfg.tmux.tavFrontWindowName
-  front = cfg.tmux.tavFrontWindowTarget
+  wname = cfg.tmux.tavWindowName
+  win = cfg.tmux.tavWindowTarget
 
-  bname = cfg.tmux.tavFrontWindowName
-  back = cfg.tmux.tavFrontWindowTarget
-
-  back = f'{sname}:$'
+  tmpsname = cfg.tmux.tavTmpSessionName
+  tmpwin = cfg.tmux.tavTmpWindowTarget
 
   width, height = getClientSize()
 
@@ -54,38 +52,35 @@ def create():
   if ! tmux has-session -t ={sname}; then
     tmux new-session        \
       -s '{sname}'          \
-      -n '{fname}'          \
+      -n '{wname}'          \
       -x '{width}'          \
       -y '{height}'         \
       -d                    \
       sh
-
-    tmux select-pane -t {front} -P bg="{cfg.colors.background}"
-    tmux send-keys -t {front} 'tav interface' c-m
-    tmux set -t "{front}" status off
-
-    sleep 1
+    tmux select-pane -t {win} -P bg="{cfg.colors.background}"
+    tmux send-keys -t {win} 'tav interface' c-m
+    tmux set -t "{win}" status off
     exit
   fi
 
-  tmux respawn-window -k -t '{back}' sh
-  if [[ $? -ne 0 ]]; then
-    tmux new-window              \
-      -a                         \
-      -t '{sname}:{{end}}'       \
-      -n '{bname}'               \
-      -d                         \
+  if ! tmux has-session -t ={tmpsname}; then
+    tmux new-session        \
+      -s '{tmpsname}'       \
+      -n '{wname}'          \
+      -x '{width}'          \
+      -y '{height}'         \
+      -d                    \
       sh
+  else
+    tmux respawn-window -k -t '{tmpwin}' sh
   fi
 
-  tmux select-pane -t {back} -P bg="{cfg.colors.background}"
-  tmux send-keys -t {back} 'tav interface' c-m
-  tmux set -t "{back}" status off
+  tmux select-pane -t {tmpwin} -P bg="{cfg.colors.background}"
+  tmux send-keys -t {tmpwin} 'tav interface' c-m
+  tmux set -t "{tmpwin}" status off
 
   sleep 1
-  tmux swap-window -d -s '{front}' -t '{back}'
-  tmux rename-window -t '{sname}:^' {fname}
-  tmux rename-window -t '{sname}:$' {bname}
+  tmux swap-window -d -s '{win}' -t '{tmpwin}'
   """
 
   shell.run(cmdstr)
