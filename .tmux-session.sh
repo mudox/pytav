0060c04baa1f24b06799020e61d1a5b8002cfc18
mudox <imudox@gmail.com>
2017-12-29 11:14:53 +0800

    Update `.tmux-session.sh`

--- .tmux-session.sh
+++ .tmux-session.sh
@@ -48,9 +48,84 @@ tmux split-window  \
   -c "${root}"
 tmux select-pane -t "${window}.1"
 
+sleep 0.2
+
+#
+# window: Server
+#
+
+jackProgress 'Creating window [Server] ...'
+
+root="${HOME}/.local/share/tav"
+window_name='Server'
+window="${session_name}:${window_name}"
+tmux new-window              \
+  -a                         \
+  -t "${session_name}:{end}" \
+  -n "${window_name}"        \
+  -c "${root}"               \
+  -d                         \
+  'tput civis; tail -f log/server.log'
+
+tmux select-pane -t "${window}.1" -d
+
+#
+# pane: Server.2
+# to the right
+# run command
+#
+
+root="${HOME}/.local/share/tav"
+tmux split-window  \
+  -t "${window}.1" \
+  -h               \
+  -l 60            \
+  -c "${root}"
+
+tmux select-pane -t "${window}.2"
+
+sleep 0.2
+
+#
+# window: Interface
+#
+
+jackProgress 'Creating window [Interface] ...'
+
+root="${HOME}/.local/share/tav"
+window_name='Interface'
+window="${session_name}:${window_name}"
+tmux new-window              \
+  -a                         \
+  -t "${session_name}:{end}" \
+  -n "${window_name}"        \
+  -c "${root}"               \
+  -d                         \
+  'tput civis; tail -f log/interface.log'
+
+tmux select-pane -t "${window}.1" -d
+
+#
+# pane: Interface.2
+# at the bottom right corner
+# for running testing commands
+#
+
+root="${HOME}/Develop/Python/tav"
+tmux split-window  \
+  -t "${window}.1" \
+  -h               \
+  -l 60            \
+  -c "${root}"
+
+tmux select-pane -t "${window}.2"
+
+sleep 0.2
+
 #
 # window: Log
 #
+
 jackProgress 'Creating window [Log] ...'
 
 root="${HOME}/.local/share/tav"
@@ -68,34 +143,20 @@ tmux select-pane -t "${window}.1" -d
 
 #
 # pane: Log.2
-# at the top right corner
-# tail the update file
-#
-
-root="${HOME}/.local/share/tav"
-tmux split-window  \
-  -t "${window}.1" \
-  -h               \
-  -l 60            \
-  -c "${root}"     \
-  'tput civis; tail -n 0 -f update'
-
-tmux select-pane -t "${window}.2" -d
-
-#
-# pane: Log.3
 # at the bottom right corner
 # for running testing commands
 #
 
 root="${HOME}/Develop/Python/tav"
 tmux split-window  \
-  -t "${window}.2" \
-  -v               \
+  -t "${window}.1" \
+  -h               \
+  -l 60            \
   -c "${root}"
 
-tmux select-pane -t "${window}.3"
+tmux select-pane -t "${window}.2"
 
+sleep 0.2
 
 jackEndProgress
 
