a7e79fd7db3c0816187c35ecfdb8c9098b2d6099
mudox <imudox@gmail.com>
2017-12-29 14:28:05 +0800

    Remove `tav hook` in favor of `tavs hook [-d|-e]`

--- src/tav/cmd.py
+++ src/tav/cmd.py
@@ -5,7 +5,7 @@ import argparse
 import logging
 from contextlib import suppress
 
-from . import core, tmux, ui
+from . import ui
 from .diagnose import diagnose
 from .server import start as startServer
 
@@ -31,20 +31,6 @@ class Command:
     while True:
       ui.show(oneshot=False)
 
-  def actionHook(self, args):
-    if args.hookOption is None:  # perform hook update
-      core.onTmuxEvent(args.event)
-
-    else:
-
-      if args.hookOption == 'enable':
-        tmux.hook.enable('invoked from command')
-      elif args.hookOption == 'disable':
-        tmux.hook.disable('invoked from command')
-      elif args.hookOption == 'print':
-        line = 'hook: ' + ('enabled' if tmux.hook.isEnabled() else 'disabled')
-        print(line)
-
   #
   # CLI interface
   #
@@ -53,16 +39,11 @@ class Command:
 
     self.parser = argparse.ArgumentParser(
         prog='tav',
-        description='An tmux `choose-tree` replacement powered by fzf action.'
-    )
+        description='An tmux `choose-tree` replacement powered by fzf action.')
     # defaults to update and choose once
     self.parser.set_defaults(func=self.actionOneshot)
 
-    self.parser.add_argument(
-        '--version',
-        action='version',
-        version=__version__
-    )
+    self.parser.add_argument('--version', action='version', version=__version__)
 
     #
     # actions
@@ -70,60 +51,14 @@ class Command:
 
     subparsers = self.parser.add_subparsers(
         title='actions',
-        description='without any action is equivalent to `oneshot`'
-    )
-
-    #
-    # action `hook`
-    #
-
-    act_hook = subparsers.add_parser(
-        'hook', aliases=['h', 'hk', 'ho'],
-        help='''
-        update snapshot and update the fzf interface in tmux window
-        '''
-    )
-    act_hook.set_defaults(func=self.actionHook, hookOption=None)
-
-    # hook {-d | -e | -p}
-    group = act_hook.add_mutually_exclusive_group()
-    group.add_argument(
-        '-d, --disable',
-        dest='hookOption',
-        action='store_const',
-        const='disable',
-        help='disable background updating triggered by tmux hooks'
-    )
-    group.add_argument(
-        '-e, --enable',
-        dest='hookOption',
-        action='store_const',
-        const='enable',
-        help='enable background updating'
-    )
-    group.add_argument(
-        '-p, --print',
-        dest='hookOption',
-        action='store_const',
-        const='print',
-        help='enable background updating'
-    )
-    group.add_argument(
-        'event',
-        nargs='?',
-        help='event type that triggers the hook')
+        description='without any action is equivalent to `oneshot`')
 
     #
     # action `oneshot`
     #
 
     act_oneshot = subparsers.add_parser(
-        'oneshot',
-        help='''
-        make a new snapshot and show the fzf interface, close after choose and
-        switch. this is the DEFAULT action
-        '''
-    )
+        'oneshot', help='the default action, show fzf inteface once')
     act_oneshot.set_defaults(func=self.actionOneshot)
 
     #
@@ -131,19 +66,14 @@ class Command:
     #
 
     act_oneshot = subparsers.add_parser(
-        'diagnose', aliases=['d', 'dia'],
-        help='dump diagnose infomation'
-    )
+        'diagnose', aliases=['d', 'dia'], help='dump diagnose infomation')
     act_oneshot.set_defaults(func=diagnose)
 
     #
     # action `server`
     #
 
-    act_server = subparsers.add_parser(
-        'server',
-        help='server mode'
-    )
+    act_server = subparsers.add_parser('server', help='start Tav server daemon')
     act_server.set_defaults(func=self.actionServer)
 
     #
@@ -151,9 +81,7 @@ class Command:
     #
 
     act_runloop = subparsers.add_parser(
-        'interface',
-        help='show the fzf inteface, remain after choose and switch.'
-    )
+        'interface', help='show the fzf inteface')
     act_runloop.set_defaults(func=self.actionInterface)
 
   def run(self):
