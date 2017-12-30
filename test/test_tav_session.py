e47dd3384682c4b8509213f59db36ff2a5fd01f8
mudox <imudox@gmail.com>
2017-12-30 23:00:47 +0800

    Update `test_tav_session.py` to test the new yin and yang

--- test/test_tav_session.py
+++ test/test_tav_session.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from tav import settings as cfg
 from tav.tmux import tavSession
 
 
 def test():
-  tavSession.refresh(forceRecreate=True)
-  assert tavSession.isReady()
-  assert tavSession.getTavWindowTTY() is not None
+  tavSession.swapYinYang(force=True)
+  assert tavSession.isYinReady()
+  assert tavSession.isYangReady()
+  assert tavSession.ttyOf(cfg.tmux.yin.target) is not None
+  assert tavSession.ttyOf(cfg.tmux.yang.target) is not None
